677a33f730d4052e12a803b207b780afa4280247
roma1n <203mine@gmail.com>
2024-05-08 17:26:32 +0800

    fix:调整点阵点的间距，相邻的块仍有0.01的间距

--- Elements/dotMatrix/dot_matrix.gd
+++ Elements/dotMatrix/dot_matrix.gd
@@ -8,11 +8,11 @@ const SOLID_DOT:PackedScene = preload("res://Elements/dotMatrix/solid_dot.tscn")
 # 当鼠标移到点的区域范围内，返回这个点的位置信息
 signal active_dot(dot_position:Vector2)
 
-const DISTANCE:float = 30.0
+const DISTANCE:float = 30.01
 
 func _ready() -> void:
 	generate_dot_matrix(curr_mold.solid_dots)
-	
+
 func generate_dot_matrix(solids:PackedVector2Array) -> void:
 	for solid in solids:
 		var node:Node2D = SOLID_DOT.instantiate()
