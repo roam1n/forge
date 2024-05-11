1c36ccfee11b2223cb23c95aafad662e79a9b7c2
Rizumu85 <Rizumu85@gmail.com>
2024-05-11 01:26:52 -0500

    加了背景

---
+++ scripts/StoreFront/commisionManager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var waiting_customer = 0
+var accepted_customer = 0
+var customer_scene = preload("res://Scenes/storeFront/customer.tscn")
+
+func _on_timer_timeout():
+	pass
+		
+		
